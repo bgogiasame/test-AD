b1911da174bf48c9ad3a3a8cc42859c2d9b004db
bgogiasame <130747554+bgogiasame@users.noreply.github.com>
2023-04-18 15:27:05 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,14 +3,27 @@ const msalConfig = {
     clientId: '94829e70-426d-44f2-922f-12fa2a96469a',
     authority: 'https://login.microsoftonline.com/f535660a-7493-4c77-8b92-5bbceef5bdcc',
     redirectUri: window.location.href // your redirect URI
+  },
+   cache: {
+    cacheLocation: 'localStorage',
+    storeAuthStateInCookie: true
   }
 };
 
-const msalInstance = new msal.PublicClientApplication(msalConfig);
+const msalInstance = new msal.UserAgentApplication(msalConfig);
 
 // Call this function when you need to get an access token
 async function getAccessToken() {
   try {
+    msalInstance = new msal.UserAgentApplication(msalConfig);
+    const accounts = msalInstance.getAllAccounts();
+
+// Print the account information for each account
+accounts.forEach((account) => {
+  console.log(`Account name: ${account.name}`);
+  console.log(`Account username: ${account.username}`);
+  console.log(`Account ID: ${account.homeAccountId}`);
+});
     const response = await msalInstance.acquireTokenSilent({
   scopes: ['user.read']
       });
